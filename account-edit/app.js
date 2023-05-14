701bcc869b0257e99d5869f77145e0cd11a373d7
abdull0ev <boburabdulloev@gmail.com>
2023-05-14 23:52:21 +0300

    addednew page

--- account-edit/app.js
+++ account-edit/app.js
@@ -33,8 +33,8 @@ let innerbtn = document.querySelector('.inner-btn');
 
 
 innerbtn.addEventListener('click', () => {
-    innerbtn.style.transform = 'translate(25px)'
-    innerbtn.style.backgroundColor = `green`
+  innerbtn.style.transform = 'translate(25px)'
+  innerbtn.style.backgroundColor = `green`
 })
 
 
@@ -42,24 +42,79 @@ innerbtn.addEventListener('click', () => {
 let openbtn = document.querySelector('.sec-btn')
 let modal = document.querySelector('.modal')
 let closebtn = document.querySelector('.close-btn')
-let openbtnchek = document.querySelector('.modal-row__btn')
-let popupchek = document.querySelector('.popur-chek')
+let openChek = document.querySelector('.form__btn')
+let popupchek = document.querySelector('.popup-chek')
 let popupCloseBtn = document.querySelector('.popup-close-btn')
 
 openbtn.addEventListener('click', () => {
-    modal.style.width = '100%';
+  modal.style.width = '100%';
 });
 
 closebtn.addEventListener('click', () => {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
+});
+
+
+popupCloseBtn.addEventListener('click', () => {
+  popupchek.style.display = 'none';
+  modal.style.display = 'none';
 })
 
-openbtnchek.addEventListener('click', () =>{
-  setTimeout(() => {
-    popupchek.style.display = 'block';
-  }, 1000);
-})
-popupCloseBtn.addEventListener('click', () =>{
- 
-  popupchek.style.display = 'none';
-})
+
+
+
+function validation(form) {
+
+  function removeError(input) {
+    const parent = input.parentNode;
+
+    if (parent.classList.contains('error')) {
+      parent.querySelector('.error-label').remove()
+      parent.classList.remove('error')
+    }
+  }
+
+  function createError(input, text) {
+    const parent = input.parentNode;
+    const errorLabel = document.createElement('label')
+
+    errorLabel.classList.add('error-label')
+    errorLabel.textContent = text
+
+    parent.classList.add('error');
+
+    parent.append(errorLabel);
+
+  }
+
+  let result = true;
+
+  const allinputs = form.querySelectorAll('input')
+
+  for (const input of allinputs) {
+
+    removeError(input)
+
+    if (input.value == "") {
+      console.log('ошибка поля');
+      createError(input, 'поля не заполнено')
+      result = false;
+    }
+
+  }
+
+  return result
+}
+
+document.querySelector('#add-form').addEventListener('submit', function (event) {
+  event.preventDefault()
+  if (validation(this)) {
+
+    setTimeout(() => {
+      openChek.addEventListener('click', () => {
+        popupchek.style.display = 'block';
+      });
+    }, 1000);
+
+  }
+});
